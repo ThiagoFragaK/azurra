53f075140507c550c1b3f241ef0a47fcd2ef5ec4
ThiagoFragaK <thiagofraga@proton.me>
2026-04-28 10:29:49 -0300

    First commit

---
+++ azurra.Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace azurra.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries =
+        [
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        ];
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public string Get()
+        {
+            return "We got the message";
+        }
+    }
+}
